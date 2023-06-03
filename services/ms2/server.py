adcb00e2f2ac50210a308da7392900f073444728
António Fernandes <antonio.mf97@gmail.com>
2023-06-03 21:05:20 +0100

    Added justfile

---
+++ services/ms2/server.py
@@ -0,0 +1,153 @@
+from concurrent import futures
+
+import grpc
+import users_pb2
+import users_pb2_grpc
+from decouple import config
+
+# from fastapi import Depends, HTTPException, status
+# from fastapi.security import OAuth2PasswordBearer
+# from jose import JWTError, jwt
+# from models import TokenData, User, UserInDB
+# from passlib.context import CryptContext
+
+# SECRET_KEY = config("SECRET_KEY")
+# ALGORITHM = config("ALGORITHM")
+# ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRE_MINUTES", cast=int)
+
+
+# fake_users_db = {
+#     "admin": {
+#         "username": "admin",
+#         "full_name": "Administrator",
+#         "email": "admin@example.com",
+#         "hashed_password": "$2b$12$JsMufKejWZVbopnQ.DksiO/OM4kU.KpQ3p7W6DTp5aQB3Mu.OqESi",
+#         "disabled": False,
+#     }
+# }
+
+
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+# def verify_password(plain_password, hashed_password):
+#     return pwd_context.verify(plain_password, hashed_password)
+
+
+# def get_password_hash(password):
+#     return pwd_context.hash(password)
+
+
+# def get_user(db, username: str):
+#     if username in db:
+#         user_dict = db[username]
+#         return UserInDB(**user_dict)
+
+
+# def authenticate_user(fake_db, username: str, password: str):
+#     user = get_user(fake_db, username)
+#     if not user:
+#         return False
+#     if not verify_password(password, user.hashed_password):
+#         return False
+#     return user
+
+
+# def create_access_token(data: dict, expires_delta: timedelta | None = None):
+#     to_encode = data.copy()
+#     if expires_delta:
+#         expire = datetime.utcnow() + expires_delta
+#     else:
+#         expire = datetime.utcnow() + timedelta(minutes=15)
+#     to_encode.update({"exp": expire})
+#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+#     return encoded_jwt
+
+
+# async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+#     try:
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+#         username: str = payload.get("sub")
+#         if username is None:
+#             raise credentials_exception
+#         token_data = TokenData(username=username)
+#     except JWTError:
+#         raise credentials_exception
+#     user = get_user(fake_users_db, username=token_data.username)
+#     if user is None:
+#         raise credentials_exception
+#     return user
+
+
+# async def get_current_active_user(
+#     current_user: Annotated[User, Depends(get_current_user)]
+# ):
+#     if current_user.disabled:
+#         raise HTTPException(status_code=400, detail="Inactive user")
+#     return current_user
+
+
+# @app.post("/token", response_model=Token)
+# async def login_for_access_token(
+#     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+# ):
+#     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+#     if not user:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Incorrect username or password",
+#             headers={"WWW-Authenticate": "Bearer"},
+#         )
+#     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+#     access_token = create_access_token(
+#         data={"sub": user.username}, expires_delta=access_token_expires
+#     )
+#     return {"access_token": access_token, "token_type": "bearer"}
+
+
+# @app.get("/users/me/", response_model=User)
+# async def read_users_me(
+#     current_user: Annotated[User, Depends(get_current_active_user)]
+# ):
+#     return current_user
+
+
+# @app.get("/users/me/items/")
+# async def read_own_items(
+#     current_user: Annotated[User, Depends(get_current_active_user)]
+# ):
+#     return [{"item_id": "Foo", "owner": current_user.username}]
+
+
+user = {
+    "user_id": "1",
+    "username": "username",
+    "usertype": "user",
+    "email": "email@email.com",
+    "disable": False,
+}
+
+
+class Users(users_pb2_grpc.UsersServicer):
+    def AuthenticateUser(self, request, context):
+        return users_pb2.UserAuthenticationReply(user=user)
+
+
+def run():
+    port = config("MS2_PORT")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    users_pb2_grpc.add_UsersServicer_to_server(Users(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    run()
