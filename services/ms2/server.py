9e574b4ba3cd283ddff59ebffdbb3fe10281f871
António Fernandes <antonio.mf97@gmail.com>
2023-06-04 11:05:09 +0100

    updated server for authentication

--- services/ms2/server.py
+++ services/ms2/server.py
@@ -4,55 +4,49 @@ import grpc
 import users_pb2
 import users_pb2_grpc
 from decouple import config
+from models import UserInDB
+from passlib.context import CryptContext
 
-# from fastapi import Depends, HTTPException, status
-# from fastapi.security import OAuth2PasswordBearer
-# from jose import JWTError, jwt
-# from models import TokenData, User, UserInDB
-# from passlib.context import CryptContext
-
-# SECRET_KEY = config("SECRET_KEY")
-# ALGORITHM = config("ALGORITHM")
-# ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRE_MINUTES", cast=int)
+ALGORITHM = config("ALGORITHM")
 
 
-# fake_users_db = {
-#     "admin": {
-#         "username": "admin",
-#         "full_name": "Administrator",
-#         "email": "admin@example.com",
-#         "hashed_password": "$2b$12$JsMufKejWZVbopnQ.DksiO/OM4kU.KpQ3p7W6DTp5aQB3Mu.OqESi",
-#         "disabled": False,
-#     }
-# }
+fake_users_db = {
+    "admin": {
+        "username": "admin",
+        "full_name": "Administrator",
+        "email": "admin@example.com",
+        "hashed_password": "$2b$12$JsMufKejWZVbopnQ.DksiO/OM4kU.KpQ3p7W6DTp5aQB3Mu.OqESi",
+        "disabled": False,
+    }
+}
 
 
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-# def verify_password(plain_password, hashed_password):
-#     return pwd_context.verify(plain_password, hashed_password)
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
 
-# def get_password_hash(password):
-#     return pwd_context.hash(password)
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 
-# def get_user(db, username: str):
-#     if username in db:
-#         user_dict = db[username]
-#         return UserInDB(**user_dict)
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
 
 
-# def authenticate_user(fake_db, username: str, password: str):
-#     user = get_user(fake_db, username)
-#     if not user:
-#         return False
-#     if not verify_password(password, user.hashed_password):
-#         return False
-#     return user
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
 
 
 # def create_access_token(data: dict, expires_delta: timedelta | None = None):
